277953992e3e7f04dc12420b6d1ccd6843548bd7
Peter Street <PeterBStreet@gmail.com>
2018-01-27 18:04:42 -0700

    Accessor, Mutators, Convert/Store

--- php/classes/Article.php
+++ php/classes/Article.php
@@ -1,76 +1,65 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: petersdata
- * @author Peter B Street <peterbstreet@gmail.com> - Code Revision
- * @author Dylan McDonald <dmcdonald21@cnm.edu> - Core code outline and format
- * @author php-fig  <http://www.php-fig.org/psr/> - PHP Standards Recommendation
- * @author Ramsey - Uuid toolset
- * Date: 1/26/18
- * Time: 8:47 AM
+/*
+ *
+ * Title Information
+ *
+ *    Created by PhpStorm.
+ *    User: petersdata
+ *    @author Peter B Street <peterbstreet@gmail.com> - Code Revision
+ *    @author Dylan McDonald <dmcdonald21@cnm.edu> - Core code outline and format
+ *    @author php-fig  <http://www.php-fig.org/psr/> - PHP Standards Recommendation
+ *    @author Ramsey - Uuid toolset
+ *    Date: 1/26/18
+ *    Time: 8:47 AM
  */
 
 /*
- * namespaces and autoload names must match
- * namespace before autoload
- * Confirm autoload as written is the correct *.psr - Later
- * Confirm the following properly sets the autoload - Later
- * How do I confirm this command was propperly issued? - Later
+ *    namespaces and autoload names must match
+ *    namespace must come before autoload
+ *    Set the namespace for objectSample object
+ *    Confirm autoload as written is the correct *.psr - Later
+ *    Confirm the following properly sets the autoload - Later
+ *    How do I confirm this command was propperly issued? - Later
  */
 namespace DataDesign;
-
 require_once("autoload.php");
 
 /*
- * Clarification needed for dirname path number of dir up to get to root project dir
+ *    The __DIR__, 2 indicates that the autoload will go up 0, 1, 2 directory layers starting with the current directory layer to load autoload.
+ *    Do we need to declare side effect?
+ *    How do I confirm this command was propperly issued? - Later
 */
 require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 /*
- * Set the namespace for objectSample object
- * Please confirm the namespace path
- * Is this where we create the class
- * Is the class name correct
- * Please confirm const is correct
- * Per php.fig best practices side effects should be declared
- * Are we following php.fig best practices?
- * Do we need to declare side effect?
- * How do I confirm this command was propperly issued?
-*/
-
-
-/*
- * Here we load Ramsey's Uuid toolset
- * How do I confirm this command was propperly issued?
+    * Here we load Ramsey's Uuid toolset
+    * How do I confirm this command was propperly issued? - Later
 */
 use Ramsey\Uuid\Uuid;
 
 /*
- * This object is based on the article table in profile.sql
- * The article table is a partial example of an article found at medium <https://medium.com>
-*/
-
-/*
- * The class is set to medium
- * This sample class does not use date therefore "use ValidateDate;" is not required
- * The article object uses userID as the and foreign primary key
- * How do I confirm this command was propperly issued?
- *  implements \JsonSerializable removed
+ *    This object is based on the article table in profile.sql
+ *    The article table is a partial example of an article found at Medium <https://medium.com>
+ *    The class is definded and set to Article
+ *    Notice Class Name only is PSR4
+ *    This sample class does not use date therefore "use ValidateDate;" is not required
+ *    The Article object uses userID as the and foreign primary key
+ *    How do I confirm this command was propperly issued? - Later
+ *    "implements \JsonSerializable" removed until later
 */
 class Article{
 	use ValidateUuid;
 /*
- * The medium class uses userID as the primary key
- * article object use userId for the primary key
- * The userId primary and foreign key should be a Uuid
- * How do I confirm this command was propperly issued?
+ *    The Article class uses userID as the primary key
+ *    The userId primary and foreign key should be a Uuid
+ *    How do I confirm this command was propperly issued?
 */
 
 /*
- * This is the articles unique ID
- * Here we set the article object's articleId state to private
- * Do I need to add @var Uuid $articleId at this time?
- * How do I confirm this command was propperly issued?
+ *    This is the articles unique ID
+ *    Here we set the article object's articleId state to private
+ *    Do I need to add @var Uuid $articleId at this time?
+ *    How do I confirm this command was propperly issued?
  */
 	private $articleId;
 
@@ -93,7 +82,7 @@ class Article{
 
 /*
  * This is the articl's title
- * Here we set the article object's title to private
+ * Here we set the article object's article title state to private
  * Do I need to add @var string $articleTitle
  * How do I confirm this command was propperly issued?
 */
@@ -101,20 +90,25 @@ class Article{
 
 /*
  * constructor for this object Article
- *
- * @param Uuid string or varchar $newarticleid is the unique articleid Uuid
- * @param Uuid string or varchar $newuserId is the unique article userId Uuid
+ * note how in class the names are camelCase and below they are UpperCamelCase(pascal) why?
+ * ? Here we construct the object article and associate the object states with?
+ * @param ? Uuid $newarticleid is the unique articleid Uuid
+ * @param ? Uuid $newuserId is the unique article userId Uuid
  * @param integer $newapproximateReadTime is the article read time in minutes
- * @param varchar $newarticleTitle is the article title
+ * @param string $newarticleTitle is the article title
  * @throws \InvalidArgumentException if data types are not valid
  * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
  * @throws \TypeError if data types violate type hints
  * @throws \Exception if some other exception occurs
- * @Documentation https://php.net/manual/en/language.oop5.decon.php
+ * @Documentation <https://php.net/manual/en/language.oop5.decon.php>
  * @throws and @Documentation notes are straight from Dylan McDonald's code template
  * Exceptions code is straight from Dylan McDonald's code
- * should Uuids be strings or varchar
+ * Why the __ in "public function__construct"
+ * ?should Uuids be strings or varchar?
  * if $new* is not null does that needed or is it implied
+ * How do the @param and @throws work? Where should they be added?
+ * Are Uuid's ints, strings?? what other types are there in PHP do they need to be stated. If stated now can we skip them in the "public function get" later
+ * where can I find info on public or function in the php documentation
 */
 	public function __construct($newArticleId, $newUserId, int $newApproximateReadTime, string $newArticleTitle) {
 		try {
@@ -123,7 +117,11 @@ class Article{
 			$this->setApproximateReadTime($newApproximateReadTime);
 			$this->setArticleTitle($newArticleTitle);
 		}
-			//determine what exception type was thrown
+
+		/*
+		 *
+		 *determine what exception type was thrown Do we really need this both here and in the mutator section?
+		*/
 		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
@@ -132,8 +130,12 @@ class Article{
 
 /*
  * accessor method for articleId
+ *
  * @return Uuid value of articleId
- * What is the Uuid component
+ *
+ * Is accessor/get the php equivilant of sql's select?
+ * Run "@return Uuid value of articleId" now or once Uuid is populated
+ * Confirm the Uuid component @return syntax
  * How do I confirm this command was propperly issued?
 */
 	public function getArticleId() : Uuid {
@@ -141,11 +143,17 @@ class Article{
 	}
 
 /*
- * mutator method for article id
+ * Mutator method for article id
+ *
+ * @param Uuid/string $newArticleId new value of articleId
+ * @throws \RangeException if $newArticleId is not positive
+ * @throws \TypeError if $newArticleId is not a uuid or string
+ *
+ * note that there may be issues with the validateUuid if the class does not have Uuids loaded into sql
  * @param, @throws, and exceptions are straight from Dylan McDonald's code
- * @param Uuid/string $newTweetId new value of tweet id
- * @throws \RangeException if $newTweetId is not positive
- * @throws \TypeError if $newTweetId is not a uuid or string
+ * @param Uuid/string $newArticleId new value of articleId
+ * @throws \RangeException if $newArticleId is not positive
+ * @throws \TypeError if $newArticleId is not a uuid or string
 */
 	public function setArticleId( $newArticleId) : void {
 		try {
@@ -154,108 +162,110 @@ class Article{
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
-
-		// convert and store the articleId
+/*
+* Convert and store the articleId
+ *
+ * Is this for uuid's only?
+*/
 		$this->articleId = $uuid;
 	}
 
 	/**
-	 * accessor method for tweet profile id
+	 * accessor method for userid
 	 *
-	 * @return Uuid value of tweet profile id
+	 * @return Uuid value of userid
 	 **/
-	public function getTweetProfileId() : Uuid{
-		return($this->tweetProfileId);
+	public function getUserId() : Uuid{
+		return($this->userId);
 	}
 
 	/**
-	 * mutator method for tweet profile id
+	 * mutator method for userid
 	 *
-	 * @param string | Uuid $newTweetProfileId new value of tweet profile id
-	 * @throws \RangeException if $newProfileId is not positive
-	 * @throws \TypeError if $newTweetProfileId is not an integer
+	 * @param Uuid/string $newArticleId new value of userId
+	 * @throws \RangeException if $newUserId is not positive
+	 * @throws \TypeError if $newUserId is not a string or uuid
 	 **/
-	public function setTweetProfileId( $newTweetProfileId) : void {
+	public function setUserId( $newUserId) : void {
 		try {
-			$uuid = self::validateUuid($newTweetProfileId);
+			$uuid = self::validateUuid($newUserId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
 
-		// convert and store the profile id
-		$this->tweetProfileId = $uuid;
+		// convert and store the userid Do we need this if no uuid?
+		$this->userId = $uuid;
 	}
 
 	/**
-	 * accessor method for tweet content
+	 * accessor method for approximateReadTime
 	 *
-	 * @return string value of tweet content
+	 * @return int value of approximateReadTime
+	 * Note how this is :int is this correct? Where :int set?
 	 **/
-	public function getTweetContent() :string {
-		return($this->tweetContent);
+	public function getApproximateReadTime() :int {
+		return($this->approximateReadTime);
 	}
 
 	/**
-	 * mutator method for tweet content
+	 * mutator method for approximateReadTime
 	 *
-	 * @param string $newTweetContent new value of tweet content
-	 * @throws \InvalidArgumentException if $newTweetContent is not a string or insecure
-	 * @throws \RangeException if $newTweetContent is > 140 characters
-	 * @throws \TypeError if $newTweetContent is not a string
+	 * @param string $newApproximateReadTime new value of approximateReadTime - use with approximateReadTime
+	 * @throws \InvalidArgumentException if $newTweetContent is not an int or insecure
+	 * @throws \RangeException if $newApproximateReadTime is > 1000
+	 * @throws \TypeError if $newApproximateReadTime is not an int
+	 * "This code verifies if the tweet content is secure" What does secure mean?
+	 *    $newTweetContent = trim($newTweetContent); Trim new tweets before writing?
+	 *    $newTweetContent = filter_var($newTweetContent, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES); Are the caps part of ramsey?
+	 *    if(empty($newTweetContent) === true) {
+	*     throw(new \InvalidArgumentException("tweet content is empty or insecure")); Is this php statment equilivant to the NOT NULL in the sql statement."CREATE TABLE user (userId BINARY(16) NOT NULL,"?
+	* "Verify the tweet content will fit in the database" Is this php statment equilivant to sql's entity type and len in the sql statement."CREATE TABLE user (userId BINARY(16) NOT NULL,"?
+	if(strlen($newTweetContent) > 140) {
+	throw(new \RangeException("tweet content too large"));
+	 * is the catch needed here?
 	 **/
-	public function setTweetContent(string $newTweetContent) : void {
-		// verify the tweet content is secure
-		$newTweetContent = trim($newTweetContent);
-		$newTweetContent = filter_var($newTweetContent, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty($newTweetContent) === true) {
-			throw(new \InvalidArgumentException("tweet content is empty or insecure"));
-		}
-
-		// verify the tweet content will fit in the database
-		if(strlen($newTweetContent) > 140) {
-			throw(new \RangeException("tweet content too large"));
-		}
-
-		// store the tweet content
-		$this->tweetContent = $newTweetContent;
-	}
-
-	/**
-	 * accessor method for tweet date
-	 *
-	 * @return \DateTime value of tweet date
-	 **/
-	public function getTweetDate() : \DateTime {
-		return($this->tweetDate);
-	}
-
-	/**
-	 * mutator method for tweet date
-	 *
-	 * @param \DateTime|string|null $newTweetDate tweet date as a DateTime object or string (or null to load the current time)
-	 * @throws \InvalidArgumentException if $newTweetDate is not a valid object or string
-	 * @throws \RangeException if $newTweetDate is a date that does not exist
-	 **/
-	public function setTweetDate($newTweetDate = null) : void {
-		// base case: if the date is null, use the current date and time
-		if($newTweetDate === null) {
-			$this->tweetDate = new \DateTime();
-			return;
-		}
-
-		// store the like date using the ValidateDate trait
-		try {
-			$newTweetDate = self::validateDateTime($newTweetDate);
-		} catch(\InvalidArgumentException | \RangeException $exception) {
+	public function setApproximateReadTime(int $newApproximateReadTime) : void {
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
-		$this->tweetDate = $newTweetDate;
+
+		/*
+		*store the tweet content
+* note there is no converstion
+		 * is conversion need for the int
+		 */
+
+		$this->approximateReadTime = $newApproximateReadTime;
 	}
 
 	/**
-	 * inserts this Tweet into mySQL
+	 * accessor method for articleTitle
+
+	 * @return string value of articleTitle
+	 *
+	 * @return \DateTime value of *namedate - sample
+	 **/
+	public function getarticleTitle() : string {
+		return($this->articleTitle);
+	}
+
+	/**
+	 * mutator method for articleTitle
+	 *
+	 * @throws \InvalidArgumentException if $newArticleTitle is not a valid string
+	 **/
+	public function setArticleTitle(string $newArticleTitle) : void {
+		 } catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+	throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	}
+		$this->articleTitle = $newArticleTitle;
+	}
+
+	/**
+	 * inserts this Article into mySQL
 	 *
 	 * @param \PDO $pdo PDO connection object
 	 * @throws \PDOException when mySQL related errors occur
@@ -264,18 +274,17 @@ class Article{
 	public function insert(\PDO $pdo) : void {
 
 		// create query template
-		$query = "INSERT INTO tweet(tweetId,tweetProfileId, tweetContent, tweetDate) VALUES(:tweetId, :tweetProfileId, :tweetContent, :tweetDate)";
+		$query = "INSERT INTO article(articleId, userId, articleTitle, articleTitle) VALUES(:articleId, :userId, :approximateReadTime, :articleTitle)";
 		$statement = $pdo->prepare($query);
 
 		// bind the member variables to the place holders in the template
-		$formattedDate = $this->tweetDate->format("Y-m-d H:i:s.u");
-		$parameters = ["tweetId" => $this->tweetId->getBytes(), "tweetProfileId" => $this->tweetProfileId->getBytes(), "tweetContent" => $this->tweetContent, "tweetDate" => $formattedDate];
+		$parameters = ["articleId" => $this->articleId->getBytes(), "userId" => $this->userId->getBytes(), "approximateReadTime" => $this->approximateReadTime, "articleTitle" => $this->articleTitle];
 		$statement->execute($parameters);
 	}
 
 
 	/**
-	 * deletes this Tweet from mySQL
+	 * deletes this article from mySQL
 	 *
 	 * @param \PDO $pdo PDO connection object
 	 * @throws \PDOException when mySQL related errors occur
@@ -284,11 +293,11 @@ class Article{
 	public function delete(\PDO $pdo) : void {
 
 		// create query template
-		$query = "DELETE FROM tweet WHERE tweetId = :tweetId";
+		$query = "DELETE FROM article WHERE articleId = :articleId";
 		$statement = $pdo->prepare($query);
 
 		// bind the member variables to the place holder in the template
-		$parameters = ["tweetId" => $this->tweetId->getBytes()];
+		$parameters = ["articleId" => $this->articleId->getBytes()];
 		$statement->execute($parameters);
 	}
 
@@ -302,12 +311,10 @@ class Article{
 	public function update(\PDO $pdo) : void {
 
 		// create query template
-		$query = "UPDATE tweet SET tweetProfileId = :tweetProfileId, tweetContent = :tweetContent, tweetDate = :tweetDate WHERE tweetId = :tweetId";
+		$query = "UPDATE article SET userId = :userId, approximateReadTime = :approximateReadTime, articleTitle = :articleTitle WHERE articleId = :articleId";
 		$statement = $pdo->prepare($query);
 
-
-		$formattedDate = $this->tweetDate->format("Y-m-d H:i:s.u");
-		$parameters = ["tweetId" => $this->tweetId->getBytes(),"tweetProfileId" => $this->tweetProfileId->getBytes(), "tweetContent" => $this->tweetContent, "tweetDate" => $formattedDate];
+		$parameters = ["articleId" => $this->articleId->getBytes(),"userId" => $this->userId->getBytes(), "approximateReadTime" => $this->approximateReadTime, "articleTitle" => $this->articleTitle];
 		$statement->execute($parameters);
 	}
 
@@ -315,40 +322,40 @@ class Article{
 	 * gets the Tweet by tweetId
 	 *
 	 * @param \PDO $pdo PDO connection object
-	 * @param Uuid|string $tweetId tweet id to search for
-	 * @return Tweet|null Tweet found or null if not found
+	 * @param Uuid|string $articleId article id to search for
+	 * @return Article|null Article found or null if not found
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError when a variable are not the correct data type
 	 **/
-	public static function getTweetByTweetId(\PDO $pdo, $tweetId) : ?Tweet {
+	public static function getArticleByArticleId(\PDO $pdo, $tweetId) : ?Article {
 		// sanitize the tweetId before searching
 		try {
-			$tweetId = self::validateUuid($tweetId);
+			$articleId = self::validateUuid($articleId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
 
 		// create query template
-		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet WHERE tweetId = :tweetId";
+		$query = "SELECT articleId, userId, approximateReadTime, articleTitle FROM article WHERE articleId = :articleId";
 		$statement = $pdo->prepare($query);
 
 		// bind the tweet id to the place holder in the template
-		$parameters = ["tweetId" => $tweetId->getBytes()];
+		$parameters = ["articleId" => $articleId->getBytes()];
 		$statement->execute($parameters);
 
 		// grab the tweet from mySQL
 		try {
-			$tweet = null;
+			$article = null;
 			$statement->setFetchMode(\PDO::FETCH_ASSOC);
 			$row = $statement->fetch();
 			if($row !== false) {
-				$tweet = new Tweet($row["tweetId"], $row["tweetProfileId"], $row["tweetContent"], $row["tweetDate"]);
+				$article = new Article($row["articleId"], $row["userId"], $row["approximateReadTime"], $row["articleTitle"]);
 			}
 		} catch(\Exception $exception) {
 			// if the row couldn't be converted, rethrow it
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
-		return($tweet);
+		return($article);
 	}
 
 	/**
